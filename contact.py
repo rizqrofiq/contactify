705c4afc9c14afbd757edae3dc71189d77c60c8f
rizqrofiq <rizqulloh.5220411394@student.uty.ac.id>
2023-03-10 08:36:53 +0700

    feat: all feature

--- contact.py
+++ contact.py
@@ -4,10 +4,10 @@ from linkedlist import LinkedList
 class Contact(LinkedList):
     def seed(self):
         for i in range(10):
-            category = "cell"
+            category = "phone"
 
             if i % 2 == 0:
-                category = "telp"
+                category = "landline"
 
             self.append({
                 "name": f"Person {i+1}",
@@ -32,3 +32,25 @@ class Contact(LinkedList):
             temp = temp.next_node
 
         return contacts
+
+    def getByName(self, name):
+        temp = self.head
+        while temp is not None:
+            if temp.data["name"] == name:
+                return temp
+
+            temp = temp.next_node
+
+        return None
+
+    def getIndexByName(self, name):
+        temp = self.head
+        iterate = 0
+        while temp is not None:
+            if temp.data["name"] == name:
+                return (iterate, temp)
+
+            temp = temp.next_node
+            iterate += 1
+
+        return (None, None)
--- main.py
+++ main.py
@@ -16,19 +16,21 @@ def main():
         print(pyfiglet.figlet_format("Contactify"))
         print("Welcome to Contactify: Your personal address book, perfected.")
 
-        print("\nHere are the menu you can choose: ")
+        print("\nHere's the menu you can choose: ")
         print("1. Show contacts list")
-        print("2. Show favorite contacts list")
-        print("3. Add contact")
-        print("4. Add contact to favorite list")
-        print("5. Update contact")
-        print("6. Delete contact\n")
+        print("2. Search Contact")
+        print("3. Show favorite contacts list")
+        print("4. Add contact")
+        print("5. Add contact to favorite list")
+        print("6. Update contact")
+        print("7. Delete contact")
+        print("8. Run Seeder\n")
 
         options = inquirer.prompt([
             inquirer.Text(
                 "option",
                 message="Choose one of the above menu [1/2..]",
-                validate=lambda _, o: 0 < int(o) <= 6
+                validate=lambda _, o: 0 < int(o) <= 8
             )
         ])
 
@@ -41,7 +43,10 @@ def main():
             if len(allContacts) == 0:
                 print("\nThere is no contacts available\n")
             else:
+                print("\nHere's all the available contacts: \n")
+
                 table = PrettyTable()
+                table.align = "l"
                 table.field_names = [
                     "No", "Name", "Contact Number", "Category"
                 ]
@@ -49,7 +54,53 @@ def main():
                 table.add_rows(allContacts)
                 print(table)
 
+        elif options == 2:
+            os.system("clear")
+
+            params = inquirer.prompt([
+                inquirer.Text("name", message="Type the name")
+            ])
+
+            c = contacts.getByName(params["name"])
+
+            if c is None:
+                print(
+                    "The contact you've search is not available! Make sure you input the correct name.")
+                input("Press any key to continue...")
+                continue
+
+            print("\nResult: \n")
+            table = PrettyTable()
+            table.field_names = ["Name", "Contact Number", "Category"]
+            table.align = "l"
+
+            table.add_row([
+                c.data["name"],
+                c.data["number"],
+                c.data["category"]
+            ])
+
+            print(table)
+
         elif options == 3:
+            os.system("clear")
+            allFavoriteContacts = favorite_contacts.getAll()
+
+            if len(allFavoriteContacts) == 0:
+                print("\nThere is no favorite contacts\n")
+            else:
+                print("\nHere's all the available favorite contacts: \n")
+
+                table = PrettyTable()
+                table.align = "l"
+                table.field_names = [
+                    "No", "Name", "Contact Number", "Category"
+                ]
+
+                table.add_rows(allFavoriteContacts)
+                print(table)
+
+        elif options == 4:
             os.system("clear")
 
             print("\nAdd new contact: ")
@@ -73,6 +124,158 @@ def main():
             })
 
             print("Contact address was successfully created")
+
+        elif options == 5:
+            os.system("clear")
+
+            params = inquirer.prompt([
+                inquirer.Text("name", message="Type the name")
+            ])
+
+            c = contacts.getByName(params["name"])
+
+            if c is None:
+                print(
+                    "The contact you've selected is not available! Make sure you input the correct name.")
+                input("Press any key to continue...")
+                continue
+
+            print("\nThis is the contact you've selected: \n")
+            table = PrettyTable()
+            table.field_names = ["Name", "Contact Number", "Category"]
+            table.align = "l"
+
+            table.add_row([
+                c.data["name"],
+                c.data["number"],
+                c.data["category"]
+            ])
+
+            print(table)
+
+            add = inquirer.confirm(
+                f"Are you sure want to add {c.data['name']}'s contact to favorites?",
+                default=False
+            )
+
+            if add:
+                favorite_contacts.append({
+                    "name": c.data["name"],
+                    "number": c.data["number"],
+                    "category": c.data["category"]
+                })
+
+                print("Contact has been succesfully added to favorite")
+            else:
+                print("Cancelled")
+
+        elif options == 6:
+            os.system("clear")
+
+            params = inquirer.prompt([
+                inquirer.Text("name", message="Type the name")
+            ])
+
+            (index, c) = contacts.getIndexByName(params["name"])
+
+            if c is None:
+                print(
+                    "The contact you've selected is not available! Make sure you input the correct name.")
+                input("Press any key to continue...")
+                continue
+
+            print("\nThis is the contact you've selected: \n")
+            table = PrettyTable()
+            table.field_names = ["Name", "Contact Number", "Category"]
+            table.align = "l"
+
+            table.add_row([
+                c.data["name"],
+                c.data["number"],
+                c.data["category"]
+            ])
+
+            print(table)
+
+            newData = inquirer.prompt([
+                inquirer.Text("name", message="New name"),
+                inquirer.Text("number", message="New Number"),
+                inquirer.List(
+                    "category",
+                    message="New category",
+                    choices=[
+                        ("Landline (Telp. Rumah)", "landline"),
+                        ("Mobile Phone", "phone")
+                    ],
+                )
+            ])
+
+            add = inquirer.confirm(
+                f"Are you sure want to update this contact?",
+                default=False
+            )
+
+            if add:
+                update = contacts.update(index, {
+                    "name": newData["name"],
+                    "number": newData["number"],
+                    "category": newData["category"]
+                })
+
+                if update:
+                    print("Contact has been successfully updated")
+                else:
+                    print("An error has occured")
+            else:
+                print("Cancelled")
+
+        elif options == 7:
+            os.system("clear")
+
+            params = inquirer.prompt([
+                inquirer.Text("name", message="Type the name")
+            ])
+
+            (index, c) = contacts.getIndexByName(params["name"])
+
+            if c is None:
+                print(
+                    "The contact you've selected is not available! Make sure you input the correct name.")
+                input("Press any key to continue...")
+                continue
+
+            print("\nThis is the contact you've selected: \n")
+            table = PrettyTable()
+            table.field_names = ["Name", "Contact Number", "Category"]
+            table.align = "l"
+
+            table.add_row([
+                c.data["name"],
+                c.data["number"],
+                c.data["category"]
+            ])
+
+            print(table)
+
+            confirm = inquirer.confirm(
+                f"Are you sure want to delete this contact?",
+                default=False
+            )
+
+            if confirm:
+                print(index)
+                delete = contacts.remove_at_index(index)
+
+                if delete:
+                    print("Contact has been successfully deleted")
+                else:
+                    print("An error has occured")
+            else:
+                print("Cancelled")
+
+        elif options == 8:
+            contacts.seed()
+            contacts.print_list()
 
         input("Press any key to continue...")
 
